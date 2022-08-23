5d3e492764b97a1aff5dce14925aae44c0d266d3
rafael <rlgdc.eng16@uea.edu.br>
2022-08-23 19:47:50 -0400

    atualizacao da questao1

--- questao1.c
+++ questao1.c
@@ -1,9 +1,20 @@
+/*
+	Rafael Lucas Gaspar Da cruz-1615100348
+	Mateus Mamede da Silva Marques-2015180069
+	Rosemiro Andrade da Silva-1515180065
+	Sergio Campos Rodrigues filho-2115200039
+	Paulo Yan Monteiro de Paiva-2015100110
+	Talles Victorio Oliveira da Silva-1915090086
+*/
+
 #include <stdio.h>
+
 int main(int argc, char **argv)
 {
 	int cont, a, maior;
 	cont=1;
 	maior=a;
+
 	while (cont <= 5)
 	{
 		printf ("digite o %d.o numero: \n", cont);
@@ -15,6 +26,8 @@ int main(int argc, char **argv)
 			maior=a;
 		}
 	}
+
 	printf("o maior e o %d \n", maior);
+
 	return 0;
 }
