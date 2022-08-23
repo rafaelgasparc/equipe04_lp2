1e8a992de8a823a7988966711253acb99bb71867
radsl.eng@uea.edu.br <rosemiro@DESKTOP-46CNV2M.localdomain>
2022-08-23 15:48:45 -0400

    altrosemiro

--- questao15.c
+++ questao15.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+int main(int argc, char **argv)
+{
+        float n, cont=2, cont2=3;
+        {
+                printf("digite um valor de termos:\n");
+                scanf("%f",&n);
+                printf("S+1/1+");
+                while(n<1)
+                {
+                        printf("%.0f/%.0f+",cont,cont2);
+                        cont=cont+1;
+                        cont2=cont2+2;
+                }
+        }
+        printf("%.0f/%.0f\n",n,cont2);
+        return 0;
+}
+
